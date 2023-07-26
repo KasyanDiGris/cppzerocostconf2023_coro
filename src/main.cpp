f31b13b6f253cb55f39f5ba5cc1ede6159307090
Konstantin Vukolov <kasyanvukolov@yandex.ru>
2023-07-26 16:13:02 +0300

    Add examples

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,53 @@
 
 #include <iostream>
 
-Task<int> Coro(std::jthread& thread) {
-    std::cout << std::this_thread::get_id() << std::endl;
-    co_await SwitchThread(thread);
-    std::cout << std::this_thread::get_id() << std::endl;
-    co_return 0;
+std::future<int> CoroReturnsFuture() {
+    co_return 100;
+}
+
+std::future<void> CoroReturnsFutureException() {
+    throw std::runtime_error("CoroReturnsFutureException failed");
+    co_return;
+}
+
+Task<int> CoroReturnsTask() {
+    co_return 200;
+}
+
+Task<void> NestedCoroWithForwardedStopToken() {
+    auto stopToken = (co_await ThisCoro).StopToken;
+
+    std::cout << "Stop token is forwarded: " << stopToken.stop_possible() << std::endl;
+}
+
+Task<void> CoroForwardsStopToken() {
+    co_await NestedCoroWithForwardedStopToken();
+}
+
+Task<void> SwitchThreadCoro(std::jthread& targetThread) {
+    std::cout << "Coro thread before switch " << std::this_thread::get_id() << std::endl;
+    co_await SwitchThread(targetThread);
+    // Prints same id until clang16
+    // because of https://github.com/llvm/llvm-project/issues/47179
+    std::cout << "Coro thread after switch " << std::this_thread::get_id() << std::endl;
 }
 
 int main() {
-    std::jthread thread;
-    Coro(thread).AsFuture().get();
+    std::cout << "CoroReturnsFuture result: " << CoroReturnsFuture().get() << std::endl;
+    try {
+        CoroReturnsFutureException().get();
+    } catch(std::runtime_error e) {
+        std::cout << e.what() << std::endl;
+    }
 
-    auto task = Coro(thread);
+    // task.AsFuture is the only way for sync wait for Task<T> result
+    std::cout << "CoroReturnsTask result: " << CoroReturnsTask().AsFuture().get() << std::endl;
+
+    std::stop_source stopSource;
+    auto task = CoroForwardsStopToken();
+    task.SetTopToken(stopSource.get_token());
+    std::move(task).AsFuture().get();
+
+    std::jthread thread;
+    SwitchThreadCoro(thread).AsFuture().get();
 }
